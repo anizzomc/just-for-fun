ac533dae0e317877d5885d929b500abcf0aef0be
Augusto Nizzo Mc Intosh <augusto.nizzo@restorando.com>
2016-10-16 02:28:20 -0300

    Test for Class Methods

--- POC/Objects.c
+++ POC/Objects.c
@@ -7,9 +7,15 @@
 
 typedef struct Object* Object_t;
 typedef struct Class* Class_t;
-typedef struct String* String_t;
+typedef struct Method *Method_t;
 
-typedef void* (*Method)(Object_t, ...);
+typedef void* (*Method_p)(Object_t, ...);
+
+struct Method {
+	char *name;
+	size_t arguments;
+	Method_p ptr;
+};
 
 struct Object {
 	Class_t class;
@@ -19,32 +25,71 @@ struct Class {
 	Class_t class;
 	Class_t superclass;
 	d_hash_t methods;
+	size_t instanceSize;
 	char *name;
+	char data[1];
 };
-
-
-struct Class MetaClass = {NULL, NULL, NULL, "Metaclass"};
-struct Class ObjectClass = { &MetaClass, NULL, NULL, "Object"};
-struct Class StringClass = { &MetaClass, &ObjectClass, NULL, "String"};
 
 struct String {
 	Class_t class;
 	char *str;
 };
+typedef struct String* String_t;
+
+struct Class MetaClass = { &MetaClass, NULL, NULL, sizeof(struct Class), "Metaclass"};
+struct Class StringMetaClass = { &StringMetaClass, &MetaClass, NULL, sizeof(struct Class), "StringMetaClass"};
+
+struct Class ObjectClass = { &MetaClass, NULL, NULL, sizeof(struct Object), "Object"};
+struct Class StringClass = { &StringMetaClass, &ObjectClass, NULL, sizeof(struct String), "String"};
+
+
+
+
 
 static void loadClasses();
-static void loadMethod(Class_t clazz, char *method, Method m);
+static void loadMethod(Class_t clazz, char *method, Method_t m);
 static unsigned strHash(char *str);
 static unsigned str_eq(char* str1, char* str2);
 
-uint32_t _resolv(Object_t obj, char *method, int super, unsigned argc, ...);
+uint32_t _resolv(Object_t obj, char *method, int super, ...);
 
-uint32_t _invoke(Method m, Object_t obj, size_t argc, uint32_t *arguments);
+Method_t _find(Class_t class, char* method);
 
-Object_t ObjectClass_new() {
-	Object_t obj = malloc(sizeof(struct Object));
-	obj->class = &ObjectClass;
+uint32_t _invoke(Method_p m, Object_t obj, size_t argc, uint32_t *arguments);
+
+Object_t MetaClass_new(Class_t class, ...) {
+	uint32_t elements[128];
+	int i;
+
+	Object_t obj = malloc(class->instanceSize);
+
+	obj->class = class; 
+
+	Method_t m = _find(class, "initialize");
+
+	if(m == NULL) {
+		printf("Method \"%s\" not found\n", "initialize");
+		abort();
+	}
+
+	va_list arg_list;
+	va_start(arg_list, class);  
+	for (i = 0 ; i < m->arguments ; i++){
+		elements[i] = va_arg(arg_list, uint32_t);
+	}
+	va_end(arg_list);
+
+	_invoke(m->ptr, obj, m->arguments, elements);
+
 	return obj;
+}
+
+char *MetaClass_name(Class_t class) {
+	return class->name;
+}
+
+void Object_initialize(Object_t this) {
+
 }
 
 char *Object_toString(Object_t this) {
@@ -57,12 +102,8 @@ int Object_equals(Object_t this, Object_t other){
 	return this == other;
 }
 
-String_t StringClass_new(char *str) {
-	String_t ret = malloc(sizeof(struct String));
-	ret->class = &StringClass;
-	ret->str = malloc(strlen(str) + 1);
-	strcpy(ret->str, str);
-	return ret;
+Class_t Object_class(Object_t this) {
+	return this->class;
 }
 
 int String_length(String_t this) {
@@ -80,28 +121,32 @@ char *String_toString(String_t this) {
 int main() {
 	loadClasses();
 
-	Object_t obj = ObjectClass_new();
-	String_t str1 = StringClass_new("Hello World");
-	String_t str2 = StringClass_new("Hello World");
-	String_t str3 = StringClass_new("Hello");
+	Object_t obj = (Object_t) _resolv(&ObjectClass, "new", 0);
+
+ 	printf("toString: %s\n", _resolv((Object_t) obj, "toString", 0));	
+ 	printf("class: %s\n", _resolv(_resolv((Object_t) obj, "class", 0), "name", 0));
+
+	// String_t str1 = StringClass_new("Hello World");
+	// String_t str2 = StringClass_new("Hello World");
+	// String_t str3 = StringClass_new("Hello");
 
 
-	printf("length: %d\n", _resolv((Object_t) str1, "length", 0, 0));
-	printf("toString: %s\n", (char*) _resolv((Object_t) str1, "toString", 0, 0));
-	printf("equals: %d\n", _resolv((Object_t) str1, "equals", 0, 1, str2));
-	printf("-----\n");
-	printf("Result: %s\n", (char*) _resolv(obj, "toString", 0, 0));
+	// printf("length: %d\n", _resolv((Object_t) str1, "length", 0));
+	// printf("toString: %s\n", (char*) _resolv((Object_t) str1, "toString", 0));
+	// printf("equals: %d\n", _resolv((Object_t) str1, "equals", 0, str2));
+	// printf("-----\n");
+	// printf("Result: %s\n", (char*) _resolv(obj, "toString", 0));
 
 }
 
-Method _find(Class_t class, char* method){
-	Method m; 
-	m = (Method) d_hash_fetch(class->methods, method);
+Method_t _find(Class_t class, char* method){
+	Method_t m; 
+	m = (Method_t) d_hash_fetch(class->methods, method);
 	return m;
 }
 
-Method _method(Class_t class, char* method){
-	Method m = _find(class, method);
+Method_t _method(Class_t class, char* method){
+	Method_t m = _find(class, method);
 
 	if(m != NULL) {
 		return m;
@@ -114,46 +159,68 @@ Method _method(Class_t class, char* method){
 	return NULL;
 }
 
-uint32_t _resolv(Object_t obj, char *method, int super, unsigned argc, ...) {
+uint32_t _resolv(Object_t obj, char *method, int super, ...) {
 	int i;
 	uint32_t elements[128];
 	va_list arg_list;
 
 	Class_t class = obj->class;
 
-	Method m = _method((super)? class->superclass : class, method);
-
-	va_start(arg_list, argc);  
-	for (i = 0 ; i < argc ; i++){
-		elements[i] = va_arg(arg_list, uint32_t);
-	}
-	va_end(arg_list);
-
+	Method_t m = _method((super)? class->superclass : class, method);
 
 	if(m == NULL) {
 		printf("Method \"%s\" not found\n", method);
 		abort();
 	}
 
-	return _invoke(m, obj, argc, elements);
+	va_start(arg_list, super);  
+	for (i = 0 ; i < m->arguments ; i++){
+		elements[i] = va_arg(arg_list, uint32_t);
+	}
+	va_end(arg_list);
+
+	return _invoke(m->ptr, obj, m->arguments, elements);
 }
+
+
+//Methods:
+struct Method MetaClass_new_m = {"new", 0, MetaClass_new};
+struct Method MetaClass_name_m = {"name", 0, MetaClass_name};
+
+struct Method Object_initialize_m = {"initialize", 0, Object_initialize};
+struct Method Object_toString_m = {"toString", 0, Object_toString};
+struct Method Object_equals_m = {"equals", 1, Object_equals};
+struct Method Object_class_m = {"class", 0, Object_class};
+
+
+struct Method String_lenght_m = {"lenght", 0, String_length};
+struct Method String_toString_m = {"toString", 0, String_toString};
+struct Method String_equals_m = {"equals", 1, String_equals};
 
 
 static void loadClasses() {
+	MetaClass.methods = d_hash_new((d_hash_fnc_t)&strHash, (d_hash_eqfnc_t) &str_eq);
+	loadMethod(&MetaClass, "new", &MetaClass_new_m);
+	loadMethod(&MetaClass, "name", &MetaClass_name_m);
+	
+
 	//Load Object
 	ObjectClass.methods = d_hash_new((d_hash_fnc_t)&strHash, (d_hash_eqfnc_t) &str_eq);
-	loadMethod(&ObjectClass, "toString", (Method) &Object_toString);
-	loadMethod(&ObjectClass, "equals", (Method) &Object_equals);
+	loadMethod(&ObjectClass, "initialize", &Object_initialize_m);
+	loadMethod(&ObjectClass, "toString", &Object_toString_m);
+	loadMethod(&ObjectClass, "equals", &Object_equals_m);
+	loadMethod(&ObjectClass, "class", &Object_class_m);
+	
 
 	//Load String
 	StringClass.methods = d_hash_new((d_hash_fnc_t)&strHash, (d_hash_eqfnc_t) &str_eq);
-	loadMethod(&StringClass, "length", (Method) &String_length);
-	loadMethod(&StringClass, "toString", (Method) &String_toString);
-	loadMethod(&StringClass, "equals", (Method) &String_equals);
+	loadMethod(&StringClass, "length", &String_length);
+	loadMethod(&StringClass, "toString", &String_toString_m);
+	loadMethod(&StringClass, "equals", &String_equals_m);
 
 }
 
-static void loadMethod(Class_t clazz, char *method, Method m){
+static void loadMethod(Class_t clazz, char *method, Method_t m){
 	d_hash_insert(clazz->methods, method, m);
 }
 
